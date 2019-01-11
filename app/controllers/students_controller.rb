de638c8a8ff61f94392d91bb41819183ba285728
jisung594 <jisung594@gmail.com>
2019-01-11 15:23:07 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,12 @@
 class StudentsController < ApplicationController
   def index
     @students = Student.all
+    render "index"
+    ##render <name of html.erb file>
+  end
+
+  def show
+    @student = Student.find(params[:id])
+    render "show"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :students, only: :index
+
+  get '/students', to: 'students#index'
+  ## get '(url)', to: '(first part of controller name)#(method/action in that controller file)'
+  get '/students/:id', to: 'students#show'
 end
